04c12af895cf45cd852fb527ce10875ebba1013f
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 16:35:13 +0800

    Add files via upload

---
+++ Class13.java
@@ -0,0 +1,33 @@
+package Java10;
+class Caaa
+{
+	public int num1;
+	public int num2;
+
+	public Caaa()
+	{
+		num1=1;
+		num2=1;
+	}
+}
+
+class Cbbb extends Caaa
+{
+	public Cbbb()
+	{
+		super();
+	}
+	public void show()
+	{
+		System.out.println("num1="+num1+", num2="+num2);
+	}
+}
+public class Class13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbbb b1=new Cbbb();
+		b1.show();
+	}
+
+}
