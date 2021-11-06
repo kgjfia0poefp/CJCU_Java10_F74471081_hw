50f5b9ec7a43faf98949c3dbbfbc29690b0820cd
kgjfia0poefp <92013723+kgjfia0poefp@users.noreply.github.com>
2021-11-06 15:33:35 +0800

    Add files via upload

---
+++ Class09.java
@@ -0,0 +1,26 @@
+package Java10;
+class Caaa
+{
+	private int num;
+
+	public void display()
+	{
+		System.out.println("printed from Caaa class");
+	}
+}
+class Cbbb extends Caaa
+{
+	public void display()
+	{
+		System.out.println("printed from Cbbb class");
+	}
+}
+public class Class09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Caaa bb=new Cbbb();
+		bb.display();
+	}
+
+}
